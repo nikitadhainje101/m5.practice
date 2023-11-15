a58c6cfb3273df52bb996cc480ac8a0d348637db
Admin <Admin@LAPTOP-QB8B4A6S>
2023-11-15 11:28:12 +0530

    demo class pushed

---
+++ src/test/java/practice/dum.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class dum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hello");
+		System.out.println("kya kr rhe ho");
+	}
+
+}
